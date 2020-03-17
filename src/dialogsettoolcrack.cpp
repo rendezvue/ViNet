c82ddd5868904aa860814fd0ea3e8be17c08e2b9
Juhong <juhong@rendezvue.ai>
2020-03-17 14:54:42 +0900

    fixed Configure Level for Crack, Color Compare(Tool Option).

--- src/dialogsettoolcrack.cpp
+++ src/dialogsettoolcrack.cpp
@@ -27,8 +27,19 @@ void DialogSetToolCrack::showEvent(QShowEvent *ev)
 {
     QDialog::showEvent(ev) ;
 
+	const int type = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Info_Get_Type(GetId()) ;
+	int inspect_level = -1 ;
+	
 	//Get Level 
-    int inspect_level = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_Crack_Get_InspectLevel(GetId());
+	if( type == TOOL_TYPE_OPTION_INSPECT_CRACK || type == TOOL_TYPE_OPTION_INSPECT_CRACK2 )
+	{
+    	inspect_level = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_Crack_Get_InspectLevel(GetId());
+	}
+	else if( type == TOOL_TYPE_OPTION_INSPECT_COLOR_COMPARE || type == TOOL_TYPE_OPTION_INSPECT_COLOR_COMPARE2 )
+	{
+		inspect_level = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_ColorCompare_Get_InspectLevel(GetId());
+	}
+	
 	//Set Slider
 	ui->horizontalSlider_feature_level->setValue(inspect_level) ;
 	ui->label_feature_level->setText(QString::number(inspect_level));
@@ -36,26 +47,36 @@ void DialogSetToolCrack::showEvent(QShowEvent *ev)
 
 void DialogSetToolCrack::OnSliderSetFeatureLevel(void)
 {
-    //get level
+	const int type = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Info_Get_Type(GetId()) ;
+
+	//get level
     int level = ui->horizontalSlider_feature_level->value() ;
-
-	qDebug("%s : SetInspectLevel = %d", __func__, level) ;
-	//set level
-    CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_Crack_Set_InspectLevel(GetId(), level);
-
-	qDebug("%s : GetInspectLevel", __func__) ;
-	
 	//Get Level 
-    int inspect_level = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_Crack_Get_InspectLevel(GetId());
-
-	qDebug("%s : GetFeatureLevel = %d", __func__, inspect_level) ;
+	int inspect_level = -1 ;
+	    
+	if( type == TOOL_TYPE_OPTION_INSPECT_CRACK || type == TOOL_TYPE_OPTION_INSPECT_CRACK2 )
+	{
+		//set level
+	    CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_Crack_Set_InspectLevel(GetId(), level);
+	
+		//Get Level 
+	    inspect_level = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_Crack_Get_InspectLevel(GetId());
+	}
+	else if( type == TOOL_TYPE_OPTION_INSPECT_COLOR_COMPARE || type == TOOL_TYPE_OPTION_INSPECT_COLOR_COMPARE2 )
+	{
+		//set level
+	    CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_ColorCompare_Set_InspectLevel(GetId(), level);
+	
+		//Get Level 
+	    inspect_level = CEnsemble::getInstance()->GetSelectDevice()->Ensemble_Tool_Option_ColorCompare_Get_InspectLevel(GetId());
+	}
 	
 	//Set Slider
     ui->horizontalSlider_feature_level->setValue(inspect_level) ;
 	ui->label_feature_level->setText(QString::number(inspect_level));
 
 	//emit UpdateToolObjectImage();
-	std::string	str_info = "Crack Level : " + std::to_string(inspect_level) ;		
+	std::string	str_info = "Level : " + std::to_string(inspect_level) ;		
 	//emit UpdateInfo(QString::fromUtf8(str_info.c_str())) ;
 }
 
